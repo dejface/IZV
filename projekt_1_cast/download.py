3ab0e1e8e11348ddf94fb32d11e81994e96c4c72
dejface <48999125+dejface@users.noreply.github.com>
2020-11-03 18:00:18 +0100

    Final version probably

--- projekt_1_cast/download.py
+++ projekt_1_cast/download.py
@@ -1,6 +1,14 @@
-import os, requests, re, zipfile, csv, io, json, pickle, gzip
+"""
+File: download.py
+Author: David Oravec (xorave05)
+Description:
+    - contains class DataDownloader, which downloads and process data about
+      accidents in Czech Republic by year
+"""
+import os, requests, re, zipfile, csv, io, pickle, gzip
 import numpy as np
 from bs4 import BeautifulSoup
+
 
 class DataDownloader:
     def __init__(self, url="https://ehw.fit.vutbr.cz/izv/", folder="data", cache_filename="data_{}.pkl.gz"):
@@ -21,55 +29,7 @@ class DataDownloader:
             "ZLK" : "15.csv",   #zlisnky
             "VYS" : "16.csv"    #vysocina
         }
-        self.processed_regions = {}
-        self.global_list = [[] for _ in range(65)]
-        self.was_downloaded = False
-
-        if os.path.isdir(folder):
-            self.folder = folder
-            self.path = os.path.abspath(folder)
-        else:
-            os.mkdir(folder)
-            self.folder = folder
-            self.path = os.path.abspath(folder)
-
-        #requests_cache.install_cache(cache_filename)
-        self.cache_filename = cache_filename
-        
-        
-
-    def download_data(self):
-        req = requests.post(self.url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'})
-        soup = BeautifulSoup(req.text, "html.parser")
-        class_a = []
-        for item in soup.find_all('a'):
-            class_a.append(item.get('href'))
-        
-        for i in range(len(class_a)):
-            if re.findall(r"^.*\.zip$", class_a[i]):
-                if os.path.exists(os.path.join(self.path,class_a[i].replace('data/',''))):
-                    continue   
-                else:    
-                    req = requests.get(self.url + class_a[i], stream=True)
-                    with open(class_a[i], 'wb') as fd:
-                        for chunk in req.iter_content(chunk_size=128):
-                            fd.write(chunk)
-        self.was_downloaded = True
-
-    def parse_region_data(self, region):
-        if self.was_downloaded is False:
-            self.download_data()
-        zip_names = os.listdir(self.folder)
-
-        oldest_files = {
-            "2016" : ("",0),
-            "2017" : ("",0),
-            "2018" : ("",0),
-            "2019" : ("",0),
-            "2020" : ("",0)
-        }
-
-        string_list = [
+        self.string_list = [
         "p1",               #int identifikacne cislo
         "p36",              #int druh pozemnej komunikacie
         "p37",              #int cislo pozemnej komunikacie
@@ -134,108 +94,158 @@ class DataDownloader:
         "s",                #int
         "t",                #str
         "p5a",              #int
-        "region"]           #str skratka regionu            
+        "region"]           #str skratka regionu  
 
-        string_index = [3,34,51,52,53,54,55,57,58,59,63,64]
-        float_index = [45,46,47,48,49,50]
+        self.processed_regions = {}         #variable to store already processed regions
+        self.global_list = [[] for _ in range(65)]
+        self.string_index = set([3,34,51,52,53,54,55,57,58,59,63,64])
+        self.float_index = set([45,46,47,48,49,50])
+
+
+        if os.path.isdir(folder):
+            self.folder = folder
+        else:
+            os.mkdir(folder)
+            self.folder = folder
+
+        zipped_data = os.listdir(self.folder)
+        pattern = re.compile(r'^.*\.zip')  
+        #storing which zip files are downloaded
+        self.zipped_data = [zipped_data[i] for i, x in enumerate(zipped_data) if pattern.search(x)]
+        pattern = re.compile(r'^.*\.gz')
+        #storing cached files
+        self.found_gz = set([zipped_data[i] for i, x in enumerate(zipped_data) if pattern.search(x)])
+        self.cache_filename = cache_filename        
         
-        """ Get the oldest files from each year for recent stats"""
-        for file in zip_names:
-            if (re.match(r"^.*(2016)\.zip$",file)):
-                size = os.path.getsize(os.path.join(self.folder,file))
-                if size > int(oldest_files["2016"][1]):
-                    oldest_files["2016"] = (file,size)
-            elif (re.match(r"^.*(2017)\.zip$",file)):
-                size = os.path.getsize(os.path.join(self.folder,file))
-                if size > int(oldest_files["2017"][1]):
-                    oldest_files["2017"] = (file,size)
-            elif (re.match(r"^.*(2018)\.zip$",file)):
-                size = os.path.getsize(os.path.join(self.folder,file))
-                if size > int(oldest_files["2018"][1]):
-                    oldest_files["2018"] = (file,size)
-            elif (re.match(r"^.*(2019)\.zip$",file)):
-                size = os.path.getsize(os.path.join(self.folder,file))
-                if size > int(oldest_files["2019"][1]):
-                    oldest_files["2019"] = (file,size)
-            elif (re.match(r"^.*(2020)\.zip$",file)):
-                size = os.path.getsize(os.path.join(self.folder,file))
-                if size > int(oldest_files["2020"][1]):
-                    oldest_files["2020"] = (file,size)
+    """ Method for downloading necessary data from 'url'. Downloading only the latest
+        zip of the year."""
+
+    def download_data(self):
+        #make a request with headers to avoid robot detection
+        req = requests.post(self.url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'})
+        soup = BeautifulSoup(req.text, "html.parser")
+
+        #parsing html and getting the latest file from each year
+        for item in soup.find_all('a'):
+            if item.contents[0] == 'ZIP':
+                if item.parent.parent.td.text.find('Prosinec') != -1:
+                    if item.get('href').replace('data/','') in self.zipped_data:
+                        continue
+                    else:
+                        self.zipped_data.append(item.get('href'))
+                elif item.parent.parent.td.text.find('2020') != -1:
+                    if len(self.zipped_data) <= 4:
+                        self.zipped_data.append(item.get('href'))
+                    else:
+                        if self.zipped_data[-1].find('2020') != -1:
+                            self.zipped_data.pop(len(self.zipped_data)-1)
+                            self.zipped_data.append(item.get('href'))
+    
+        #if the zip file already exists, do nothing, else download zip
+        for i in range(len(self.zipped_data)):
+            if os.path.exists(os.path.join(self.folder,self.zipped_data[i].replace('data/',''))):
+                continue   
+            else:    
+                req = requests.get(self.url + self.zipped_data[i], stream=True)
+                with open(self.zipped_data[i], 'wb') as fd:
+                    for chunk in req.iter_content(chunk_size=128):
+                        fd.write(chunk)
+
+    """ Method for parsing specific region data. Returns tuple(list_of_headers,list_of_nparrays)"""
+
+    def parse_region_data(self, region):
+        #if any of .zip data files is missing, download it
+        if len(self.zipped_data) != 5:
+            self.download_data()
 
         list_of_np_arrays = []
         temp_arrays = [[] for _ in range(65)]
-
-        for i in oldest_files:
-            with zipfile.ZipFile(os.path.join(self.path,oldest_files[i][0])) as zf:
+        
+        for j in range(len(self.zipped_data)):
+            with zipfile.ZipFile(os.path.join(self.folder,self.zipped_data[j].replace('data/',''))) as zf:
                 with zf.open(self.regions[region],'r') as f:
-                    reader = csv.reader(io.TextIOWrapper(f,'ISO 8859-2'),delimiter=';')
+                    reader = csv.reader(io.TextIOWrapper(f,'windows-1250'),delimiter=';')
                     for line in reader:
                         for i in range(len(line)+1):
-                            try:
-                                if i == 6:
-                                    hours = int(line[i][:2])
-                                    minutes = int(line[i][2:])
-                                    if hours > 24 or minutes > 59:
-                                        line[i] = '-1'
-                                if i not in string_index and i not in float_index:
-                                    try:
-                                        line[i] = int(line[i])
-                                    except:
-                                        line[i] = -1
-                                elif i in float_index:
-                                    try:
-                                        line[i] = float(line[i].replace(',','.'))
-                                    except:
-                                        line[i] = '-1.0'
-                                if i == 64:
-                                    temp_arrays[i].append(region)
-                                    self.global_list[i].append(region)
-                                else:
-                                    temp_arrays[i].append(line[i])
-                                    self.global_list[i].append(line[i])
-                            except:
-                                pass
+                            if i == 5:  #hour column
+                                hours = int(line[i][:2])
+                                minutes = int(line[i][2:])
+                                if hours > 24 or minutes > 59:  #invalid hour or minute
+                                    line[i] = '-1'
+                            if i not in self.string_index and i not in self.float_index:
+                                try:
+                                    line[i] = int(line[i])
+                                except:
+                                    line[i] = -1
+                            elif i in self.float_index:
+                                try:
+                                    line[i] = float(line[i].replace(',','.'))
+                                except:
+                                    line[i] = '-1.0' 
+                            if i == 64: #appending region to list
+                                temp_arrays[i].append(region)
+                                self.global_list[i].append(region)
+                            else:
+                                temp_arrays[i].append(line[i])
+                                self.global_list[i].append(line[i])
+
+        #change every list to numpy array and specify its type
 
         for i in range(len(temp_arrays)):
-            list_of_np_arrays.append(np.array(temp_arrays[i]))
+            if i == 3:
+                arr = np.array(temp_arrays[i],dtype='datetime64')
+            elif i not in self.string_index and i not in self.float_index:
+                arr = np.array(temp_arrays[i],dtype='int64')
+            elif i in self.float_index:
+                arr = np.array(temp_arrays[i],dtype='float64')
+            else:
+                arr = np.array(temp_arrays[i])
+            list_of_np_arrays.append(arr)
 
-        return (string_list, list_of_np_arrays)
+        return (self.string_list, list_of_np_arrays)
+
+    """ Method for getting specific region data. Returns tuple(list_of_headers,list_of_nparrays)"""
 
     def get_list(self, regions = None):
-        dirs = os.listdir(self.folder)
-        found_gz = [dirs[i] for i, x in enumerate(dirs) if re.findall(r'^.*\.gz',x)]
-        found = False
         result = []
+
+        #if none of regions are specified, make a full list of regions in CR
+
         if regions is None:
-            regions = []
-            for region in self.regions.keys():
-                regions.append(region)
+            regions = list(self.regions.keys())    
 
         for i in range(len(regions)):
-            found = False
-            if regions[i] in self.processed_regions:
+            curr_region = regions[i]
+
+            #if current region is already processed in memory, continue
+
+            if curr_region in self.processed_regions:
                 continue
-            elif regions[i] not in self.processed_regions: #ak region neni v atribute v pamati
-                pattern = re.compile('data_{}.pkl.gz'.format(regions[i]))   #spravim pattern na hladanie suborov v cache
-                for x in range(len(found_gz)):
-                    if (re.match(pattern,found_gz[x])): #ak je pattern rovnaky ako subor
-                        cached_file = gzip.open(os.path.join(self.folder,found_gz[x]),'rb') #otvaram subor
-                        self.processed_regions[regions[i]] = pickle.load(cached_file)
-                        cached_file.close()
-                        found = True
-                        for j in range(len(self.processed_regions[regions[i]][1])):
-                            self.global_list[j] += self.processed_regions[regions[i]][1][j].tolist()
-                if (found is False):
-                    header, data = self.parse_region_data(regions[i])
-                    cached_file = gzip.open(os.path.join(self.folder,self.cache_filename.format(regions[i])),'wb')
-                    pickle.dump((header, data),cached_file)
-                    cached_file.close()
-                    self.processed_regions[regions[i]] = (header,data)
-
+            elif curr_region not in self.processed_regions:     #if region is not processed yet
+                pattern = self.cache_filename.format(curr_region) 
+                if pattern in self.found_gz:    #if region is in cache, load it 
+                    with gzip.open(os.path.join(self.folder,pattern),'rb') as f: 
+                        self.processed_regions[curr_region] = pickle.load(f)
+                    for j in range(len(self.processed_regions[curr_region][1])):
+                        self.global_list[j] += self.processed_regions[curr_region][1][j].tolist()
+                else:   #else process it
+                        data = self.parse_region_data(curr_region)
+                        with gzip.open(os.path.join(self.folder,pattern),'wb') as f:
+                            pickle.dump(data,f)
+                        self.processed_regions[curr_region] = data
+        
         for k in range(len(self.global_list)):
-            result.append(np.array(self.global_list[k]))
+            if k == 3:
+                arr = np.array(self.global_list[k],dtype='datetime64')
+            elif k not in self.string_index and k not in self.float_index:
+                arr = np.array(self.global_list[k],dtype='int64')
+            elif k in self.float_index:
+                arr = np.array(self.global_list[k],dtype='float64')
+            else:
+                arr = np.array(self.global_list[k])
+            result.append(arr)
 
-        return (self.processed_regions[regions[0]][0],result)
+        return (self.string_list,result)
 
 if __name__ == "__main__":
     my_class = DataDownloader().get_list(["PLK","PAK","KVK"])
